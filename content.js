22d64634c5297acb8bd91480047112042d42727a
Manish Motiani <manikmanshu@gmail.com>
2016-04-12 17:29:44 +0530

    Add Redirection to google

--- content.js
+++ content.js
@@ -6,4 +6,6 @@ var isFacebook = "facebook.com";
 
 if (hrefFB.indexOf(isFacebook) >= 0) {
     alert('Facebook Found');
+    // Redirect to Google
+    window.location.replace("http://google.com");
 }
